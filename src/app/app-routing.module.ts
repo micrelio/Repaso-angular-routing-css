4e61f2ab62f69479a113a5c715daf17d2503aa32
micrelio <microa4@gmail.com>
2019-06-19 18:53:29 +0200

    acabado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { EstudiantesComponent } from './estudiantes/estudiantes.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'inicio', pathMatch: 'full' },
   {path: 'inicio', component: InicioComponent },
+  { path: 'estudiantes', component: EstudiantesComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+      selector: 'app-root',
+      templateUrl: './app.component.html',
+      styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pruebas2-routing-css';
+      title:string = 'Tutorial Angular 7 #3: Enrutamiento';
+      isActive:boolean=true;
+  toggle():void{
+    this.isActive=!this.isActive;
+  }
 }
